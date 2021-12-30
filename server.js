0faf416349f4d736197f776ff001233d7a3b5f6f
elliott dombrowski <elliottroyal@me.com>
2021-12-30 14:41:10 -0600

    still testing git

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
-//why is this being required if its not being used?
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
