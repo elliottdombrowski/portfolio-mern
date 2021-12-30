a76713b1360603737bbb3952f414ea75eea9bda2
elliott dombrowski <elliottroyal@me.com>
2021-12-30 14:42:41 -0600

    one more git test

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
-const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const sgMail = require('@sendgrid/mail')
