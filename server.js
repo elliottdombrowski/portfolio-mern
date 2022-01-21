bc602c65107850d311a01891098aeae2b0ee8135
elliott dombrowski <elliottroyal@me.com>
2022-01-21 16:13:42 -0600

    updated server.js attempting heroku deployment

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+};
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
+});
+
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
