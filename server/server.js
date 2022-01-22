d456785d8a319cf7eaa135e09e90c094506a3506
elliott dombrowski <elliottroyal@me.com>
2022-01-21 19:53:35 -0600

    forgot to add process.env.port.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
