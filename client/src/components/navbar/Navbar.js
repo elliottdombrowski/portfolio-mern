9fa30f76d4a0ce0f2195468eaee35c636eb2d656
elliott dombrowski <elliottroyal@me.com>
2022-02-20 11:57:56 -0600

    removed social media links from navbar

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -3,17 +3,17 @@ import './navbar.css';
 import './query.css';
 import $ from 'jquery';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core'; 
+import { library } from '@fortawesome/fontawesome-svg-core';
 import { faGithub, faLinkedin, faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { faBell } from '@fortawesome/free-solid-svg-icons';
 
 library.add(
-    faGithub,
-    faLinkedin,
-    faFacebook,
-    faInstagram,
-    faTwitter,
-    faBell
+  faGithub,
+  faLinkedin,
+  faFacebook,
+  faInstagram,
+  faTwitter,
+  faBell
 );
 
 const github = <FontAwesomeIcon icon={faGithub} />
@@ -24,113 +24,104 @@ const twitter = <FontAwesomeIcon icon={faTwitter} />
 const bell = <FontAwesomeIcon icon={faBell} />
 
 function Navbar({ curPage, handlePageChange }) {
-    const mobileMenu = () => {
-        document.getElementById('burger').classList.toggle('active');
-        document.getElementById('nav-right').classList.toggle('active');
-    };
-    
-    const navClick = () => {
-      let nav = document.getElementById('nav-right');
-      let burger = document.getElementById('burger');
-      if (nav.classList.contains('active')) {
-        nav.classList.remove('active');
-        burger.classList.remove('active');
-      };
-    };
+  const mobileMenu = () => {
+    document.getElementById('burger').classList.toggle('active');
+    document.getElementById('nav-right').classList.toggle('active');
+  };
 
-    //IF ANYWHERE ON WINDOW IS CLICKED EXECPT FOR HAMBURGER MENU, NAVBAR WILL CLOSE.
-    //HAMBURGER LOGIC STILL CLOSES ON CLICK
-    window.onclick = (event) => {
-      if (event.target !== document.getElementById('burger') && event.target !== document.getElementById('bar-one') && event.target !== document.getElementById('bar-two') && event.target !== document.getElementById('bar-three')) {
-        document.getElementById('burger').classList.remove('active');
-        document.getElementById('nav-right').classList.remove('active');
-      }
+  const navClick = () => {
+    let nav = document.getElementById('nav-right');
+    let burger = document.getElementById('burger');
+    if (nav.classList.contains('active')) {
+      nav.classList.remove('active');
+      burger.classList.remove('active');
     };
+  };
 
-    //NOT CURRENTLY WORKING- BUT NOT CAUSING PROBLEMS.
-    window.onscroll = () => {
+  //IF ANYWHERE ON WINDOW IS CLICKED EXECPT FOR HAMBURGER MENU, NAVBAR WILL CLOSE.
+  //HAMBURGER LOGIC STILL CLOSES ON CLICK
+  window.onclick = (event) => {
+    if (event.target !== document.getElementById('burger') && event.target !== document.getElementById('bar-one') && event.target !== document.getElementById('bar-two') && event.target !== document.getElementById('bar-three')) {
       document.getElementById('burger').classList.remove('active');
       document.getElementById('nav-right').classList.remove('active');
+    }
+  };
+
+  //NOT CURRENTLY WORKING- BUT NOT CAUSING PROBLEMS.
+  window.onscroll = () => {
+    document.getElementById('burger').classList.remove('active');
+    document.getElementById('nav-right').classList.remove('active');
+  };
+
+  const toggleMedia = () => {
+    let bell = document.getElementById('bell');
+    let media = document.getElementById('media-show');
+    bell.classList.toggle('active');
+    media.classList.toggle('active');
+    if (media.classList.contains('media-hide')) {
+      $('#media-show').fadeIn(1000);
+      media.classList.add('media-fade-in');
+      media.classList.remove('media-hide');
+      return;
+    }
+    if (bell.classList.contains('active')) {
+      $('#media-show').fadeIn(1000);
+    } else if (!bell.classList.contains('active')) {
+      $('#media-show').fadeOut(1000);
     };
+  };
 
-    const toggleMedia = () => {
-        let bell = document.getElementById('bell');
-        let media = document.getElementById('media-show');
-        bell.classList.toggle('active');
-        media.classList.toggle('active');
-        if (media.classList.contains('media-hide')) {
-            $('#media-show').fadeIn(1000);
-            media.classList.add('media-fade-in');
-            media.classList.remove('media-hide');
-            return;
-        }
-        if (bell.classList.contains('active')) {
-            $('#media-show').fadeIn(1000);
-        } else if (!bell.classList.contains('active')) {
-            $('#media-show').fadeOut(1000);
-        };
-    };
-
-    return (
-        <div className='navbar-wrapper'>
-            <div className='navbar-left'>
-                <div className='bell-wrapper' id="bell">
-                    <i className="fa-2x icon-hvr btn-hvr" onClick={() => toggleMedia()}>{bell}</i>
-                </div>
-
-                <div className='social-icons media media-hide' id="media-show">
-                    <a target="_blank" rel="noreferrer" href="https://github.com/elliottdombrowski">
-                        <i className="fa-2x media-nav icon-hvr btn-hvr">{github}</i>
-                    </a>
-                    <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/elliott-dombrowski-8b2ba8211/">
-                        <i className="fa-2x media-nav icon-hvr btn-hvr">{linkedin}</i>
-                    </a>
-                    <a target="_blank" rel="noreferrer" href="https://www.facebook.com/elliott.dombrowski/">
-                        <i className="fa-2x media-nav icon-hvr btn-hvr">{facebook}</i>
-                    </a>
-                    <a target="_blank" rel="noreferrer" href="https://www.instagram.com/_roger_workman_/">
-                        <i className="fa-2x media-nav icon-hvr btn-hvr">{instagram}</i>
-                    </a>
-                    <a target="_blank" rel="noreferrer" href="https://twitter.com/dupagewasteland">
-                        <i className="fa-2x media-nav icon-hvr btn-hvr">{twitter}</i>
-                    </a>
-                </div>
-            </div>
-
-            <div className='navbar-right' id="nav-right">
-                <a 
-                    href="#about"
-                    onClick={() => {
-                        handlePageChange('About');
-                        navClick();
-                    }}>
-                    <p className='nav-item btn-hvr'>about</p>
-                </a>
-                <a 
-                    href="#work"
-                    onClick={() => {
-                        handlePageChange('Work');
-                        navClick();
-                    }}>
-                    <p className='nav-item btn-hvr'>work</p>
-                </a>
-                <a 
-                    href="#contact"
-                    onClick={() => {
-                        handlePageChange('Contact');
-                        navClick();
-                    }}>
-                    <p className='nav-item btn-hvr'>contact</p>
-                </a>
-            </div>
-
-            <div className='hamburger' id="burger" onClick={() => mobileMenu()}>
-                <span className="bar" id='bar-one'></span>
-                <span className="bar" id='bar-two'></span>
-                <span className="bar" id='bar-three'></span>
-            </div>
+  return (
+    <div className='navbar-wrapper'>
+      <div className='navbar-left'>
+        <div className='bell-wrapper' id="bell">
+          <i className="fa-2x icon-hvr btn-hvr" onClick={() => toggleMedia()}>{bell}</i>
         </div>
-    );
+
+        <div className='social-icons media media-hide' id="media-show">
+          <a target="_blank" rel="noreferrer" href="https://github.com/elliottdombrowski">
+            <i className="fa-2x media-nav icon-hvr btn-hvr">{github}</i>
+          </a>
+          <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/elliott-dombrowski-8b2ba8211/">
+            <i className="fa-2x media-nav icon-hvr btn-hvr">{linkedin}</i>
+          </a>
+        </div>
+      </div>
+
+      <div className='navbar-right' id="nav-right">
+        <a
+          href="#about"
+          onClick={() => {
+            handlePageChange('About');
+            navClick();
+          }}>
+          <p className='nav-item btn-hvr'>about</p>
+        </a>
+        <a
+          href="#work"
+          onClick={() => {
+            handlePageChange('Work');
+            navClick();
+          }}>
+          <p className='nav-item btn-hvr'>work</p>
+        </a>
+        <a
+          href="#contact"
+          onClick={() => {
+            handlePageChange('Contact');
+            navClick();
+          }}>
+          <p className='nav-item btn-hvr'>contact</p>
+        </a>
+      </div>
+
+      <div className='hamburger' id="burger" onClick={() => mobileMenu()}>
+        <span className="bar" id='bar-one'></span>
+        <span className="bar" id='bar-two'></span>
+        <span className="bar" id='bar-three'></span>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
