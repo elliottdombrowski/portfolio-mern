d44df9f9da8d5950b4fdde5bbb91e01dd333fb06
elliott dombrowski <elliottroyal@me.com>
2022-02-27 14:25:40 -0600

    added framer-motion and react-router-dom packages

---
+++ client/src/AnimatePage.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
+import './App.scss';
+
+const animation = {
+  initial: {opacity: 0},
+  animate: {opacity: 1},
+  exit: {opacity: 0},
+};
+
+const AnimatePage = ({ children }) => {
+  return (
+    <>
+      <motion.div
+        variants={animation}
+        initial='initial'
+        animate='animate'
+        exit='exit'
+        transition={{ duration: 1.2 }}
+        className='animate-page'
+      >
+        {children}
+      </motion.div>
+    </>
+  );
+};
+ 
+export default AnimatePage;
