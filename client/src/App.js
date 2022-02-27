48bb401c8a3a7e6f506f90fa8091025df80a374e
elliott dombrowski <elliottroyal@me.com>
2022-02-27 15:15:36 -0600

    deleted all files to try and reset

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,6 @@ import { Routes, Route, useLocation } from 'react-router-dom';
 
 import { AnimatePresence } from 'framer-motion';
 
-// import Homepage from './components/homepage/Homepage';
-// import Navbar from './components/navbar/Navbar';
-// import Footer from './components/footer/Footer';
-// import About from './components/about/About';
-// import Work from './components/work/Work';
-// import Contact from './components/contact/Contact';
-// import Container from './components/Container';
-
-import Navbar from './components/Navbar/Navbar.js';
-import Footer from './components/Footer/Footer.js';
-import Homepage from './pages/Homepage/Homepage.js';
-import About from './pages/About/About.js';
-import Work from './pages/Work/Work.js';
-import Contact from './pages/Contact/Contact.js';
-
 import './App.scss';
 
 // const App = () => <Container />;
