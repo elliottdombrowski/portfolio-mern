1dc734abf3e9d24caa0760e52e5799e3bf01aa85
elliott dombrowski <elliottroyal@me.com>
2022-03-01 20:17:09 -0600

    replaced homepage icon w/ bell

--- client/src/components/navleft/NavLeft.js
+++ client/src/components/navleft/NavLeft.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBell } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
-const github = <FontAwesomeIcon icon={faGithub} className='fa-2x github-icon' />
+const bell = <FontAwesomeIcon icon={faBell} className='fa-2x fa-regular' />
 const linkedIn = <FontAwesomeIcon icon={faLinkedin} id='nav-linkedin' className='fa-xl navbar-icons linkedin-icon' />
-const githubOption = <FontAwesomeIcon icon={faGithub} id='nav-github' className='fa-xl navbar-icons github-small-icon' />
+const github = <FontAwesomeIcon icon={faGithub} id='nav-github' className='fa-xl navbar-icons github-small-icon' />
+
 
 const NavLeft = () => {
   
@@ -22,13 +24,13 @@ const NavLeft = () => {
           className='nav-item'
           onClick={showNavItems}
         >
-          {github}
+          {bell}
         </li>
         <li className='nav-item'>
           {linkedIn}
         </li>
         <li className='nav-item'>
-          {githubOption}
+          {github}
         </li>
       </ul>
     </>
